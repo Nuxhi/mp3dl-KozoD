4b371d93565e27e81265c7f27c7a42dfd1b8804e
Nuxhi <78807626+Nuxhi@users.noreply.github.com>
2022-01-14 20:28:12 +0100

    Update KozoD.py

--- KozoD.py
+++ KozoD.py
@@ -2,6 +2,8 @@
 #Made in France
 #All rights reserved to Nuxhi, no modified copy of KozoD is allowed, if you wish to contradict the development contact me here:
 
+#KozoD-V1.0.1
+
 #Discord : Nuxhi#4601
 #In the menu, done "/Nuxhi" for more information
 
