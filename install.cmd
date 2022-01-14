bd5c360e2316884e25504e8782bee32b90e5c520
Nuxhi <78807626+Nuxhi@users.noreply.github.com>
2022-01-14 18:54:00 +0100

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,21 +1,24 @@
 @echo off
+Mode Con Cols=125 Lines=30
+color 8
 echo [KozoD] Welcome to KozoVideoDownloader installeur.
 @ping localhost -n 2 >nul
-
+echo.
+echo.
 echo [KozoD] [Console] Python 3 requires.
-
-
 echo [KozoD] [Console] Pip upgrade.
 @ping localhost -n 1 >nul
 py -m pip install --upgrade pip
 
-
+echo.
 echo [KozoD] [Console] Installation of pytube lib.
 @ping localhost -n 1 >nul
 py -m pip install pytube
-
-
+py -m pip install --upgrade pytube
 echo.
-echo press a key to launch KozoD
-pause > nul
+echo.
+echo [KozoD] [Tchek] All KozoD components were successfully installed.
+echo Launch in 8s !
+color 0f 
+TIMEOUT /T 8 /NOBREAK
 start KozoD.py
